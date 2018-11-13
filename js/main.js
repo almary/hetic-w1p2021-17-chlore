951f3879d26f510c17b3182b513c5fce8169fba2
almary <alexandre.mary.adenni@gmail.com>
2018-11-13 17:57:16 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ var creator = document.getElementById("creator");
 var bio = document.getElementById("bio");
 creator.addEventListener("click", function() {
   bio.classList.toggle("open");
+  arrowblack.classList.toggle("open");
 });
