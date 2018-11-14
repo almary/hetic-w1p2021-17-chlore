23ebef9db97d3367c62feed243d662295837b890
almary <alexandre.mary.adenni@gmail.com>
2018-11-14 10:11:33 +0100

    update html/css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var creator = document.getElementById("creator");
 var bio = document.getElementById("bio");
 creator.addEventListener("click", function() {
-  bio.classList.toggle("open");
-  arrowblack.classList.toggle("open");
+  bio.classList.toggle("is-open");
+  arrowb.classList.toggle("is-open");
 });
