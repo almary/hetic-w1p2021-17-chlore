04884eaf8ff0d489403bb511aa686259665c2ca9
almary <alexandre.mary.adenni@gmail.com>
2018-11-14 17:38:40 +0100

    1.5 update

--- js/product.js
+++ js/product.js
@@ -1,21 +1,23 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+if (window.matchMedia("(max-width: 1150px)").matches) {
+  var slideIndex = 1;
+  showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
+  function plusDivs(n) {
+    showDivs((slideIndex += n));
+  }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  if (n > x.length) {
-    slideIndex = 1;
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
   }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex - 1].style.display = "block";
 }
